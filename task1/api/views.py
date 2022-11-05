a4af6b02c7335acdc25a3180f523de564a03d6c9
Toheeb <aladetoheeb5@gmail.com>
2022-11-05 13:41:26 +0100

    Add calculation

--- task1/api/views.py
+++ task1/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+import json
+
 # Create your views here.
 
 @api_view(['GET'])
@@ -12,10 +14,21 @@ def get_data(request):
 @api_view(['GET', 'POST'])
 def calculate(request):
     if (request.method=='POST'):
-        data = request.POST
+        data = json.loads(request.body.decode())
         operation_type = data.get('operation_type')
-        print(operation_type)
-        return Response(operation_type)
+        x = int(data.get('x'))
+        y = int(data.get('y'))
+
+        if operation_type == "addition":
+            res = x + y
+        if operation_type == "subtraction":
+            res = x - y
+        if operation_type == "multiplication":
+            res = x * y
+        
+
+        data = {"slackUsername": "Horlarmmy", "result": res, "operation_type": operation_type}
+        return Response(data)
 
     else:
         return Response("You no go like perform a POST request ???")
